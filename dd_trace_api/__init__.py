68239981489aed54b55e88463a219774791697ac
Emmett Butler <emmett.butler321@gmail.com>
2025-01-16 13:55:05 -0800

    working on static methods

--- dd_trace_api/__init__.py
+++ dd_trace_api/__init__.py
@@ -20,8 +20,6 @@ __all__ = [
     "filters",
     "pin",
     "provider",
-    "sampling_rule",
-    "sampler",
     "span",
     "tracer",
 ]
@@ -55,6 +53,7 @@ class _Stub:
                         for name, info in method_info.get("kwargs", {}).items()
                     },
                     return_info=("None", "Any"),
+                    static=method_info.get("static", False),
                 ),
             )
 
@@ -70,25 +69,32 @@ class _CallableStub(_Stub):
         return_info: Tuple[str, str] = ("None", "Any"),
         posargs: Optional[List[Tuple[str, str]]] = None,
         kwargs: Optional[Dict[Any, str]] = None,
+        static: bool = False,
         *args,
     ):
         super(_CallableStub, self).__init__(*args)
         self.return_info = return_info
         self.posargs = posargs
         self.kwargs = kwargs
+        self.static = static
 
     def __call__(self, *args, **kwargs):
         if not hasattr(self, f"_inner_{self._public_name}"):
-            posarg_defs = ", " + ", ".join(f"{arg}: {_type}" for arg, _type in self.posargs) if self.posargs else ""
+            posarg_defs = ", ".join(f"{arg}: {_type}" for arg, _type in self.posargs) if self.posargs else ""
             kwarg_defs = (
-                ", " + ", ".join(f"{arg}:{_type}={default.__repr__()}" for arg, (default, _type) in self.kwargs.items())
+                ", ".join(f"{arg}:{_type}={default.__repr__()}" for arg, (default, _type) in self.kwargs.items())
                 if self.kwargs
                 else ""
             )
+            self_param = "self" if self.static else ""
+            params = ", ".join(self_param + posarg_defs + kwarg_defs)
             code = f"""
-def _inner_{self._public_name}(self{posarg_defs}{kwarg_defs}) -> {self.return_info[1]}:
+{'@staticmethod' if self.static else ''}
+def _inner_{self._public_name}({params}) -> {self.return_info[1]}:
     return {self.return_info[0]}
-
+"""
+            if not self.static:
+                code += f"""
 bound_method = _inner_{self._public_name}.__get__(self, self.__class__)
 setattr(self, '_inner_{self._public_name}', bound_method)
 """
@@ -114,38 +120,6 @@ class Tracer(_Stub):
     pass
 
 
-class _SamplingRule:
-    __slots__ = ["sample_rate", "matches", "tags_match", "check_tags", "sample", "choose_matcher"]
-
-
-class _SamplingError:
-    pass
-
-
-class _BaseSampler:
-    __slots__ = ["sample"]
-
-
-class _BasePrioritySampler:
-    __slots__ = ["update_rate_by_service_sample_rates"]
-
-
-class _AllSampler:
-    __slots__ = ["sample"]
-
-
-class _RateSampler:
-    __slots__ = ["sample", "set_sample_rate"]
-
-
-class _RateByServiceSampler:
-    __slots__ = ["sample", "set_sample_rate", "update_rate_by_service_sample_rates"]
-
-
-class _DatadogSampler:
-    __slots__ = ["default_sample_rate", "limiter", "sample", "update_rate_by_service_sample_rates"]
-
-
 class _BaseContextProvider:
     __slots__ = ["activate", "active"]
 
@@ -166,6 +140,10 @@ class Pin(_Stub):
     pass
 
 
+class HTTPPropagator(_Stub):
+    pass
+
+
 class _TraceFilter:
     __slots__ = ["process_trace"]
 
@@ -176,14 +154,7 @@ class _FilterRequestsOnUrl:
 
 class _Context:
     __slots__ = [
-        "sampling_priority",
-        "trace_id",
-        "span_id",
-        "dd_origin",
-        "dd_user_id",
         "set_baggage_item",
-        "get_baggage_item",
-        "get_all_baggage_items",
         "remove_baggage_item",
         "remove_all_baggage_items",
     ]
@@ -205,22 +176,6 @@ class provider:
     __slots__ = ["BaseContextProvider", "DatadogContextMixin", "DefaultContextProvider", "CIContextProvider"]
 
 
-class sampler:
-    __slots__ = [
-        "BaseSampler",
-        "BasePrioritySampler",
-        "AllSampler",
-        "RateSampler",
-        "RateByServiceSampler",
-        "DatadogSampler",
-        "SamplingError",
-    ]
-
-
-class sampling_rule:
-    __slots__ = ["SamplingRule"]
-
-
 class span:
     __slots__ = ["Span"]
 
@@ -229,5 +184,3 @@ tracer = _Stub()
 tracer.Tracer = Tracer
 pin = _Stub()
 pin.Pin = Pin
-
-# TODO - add API from dd-trace-py/ddtrace/propagation/__init__.py
--- test/test_api.py
+++ test/test_api.py
@@ -11,14 +11,16 @@ with open(os.path.join(os.path.dirname(dd_trace_api.__file__), "api.yaml")) as d
 
 def test_api_accessible():
     for class_name, class_info in definition["classes"].items():
-        instance = getattr(dd_trace_api, class_name)()
+        _class = getattr(dd_trace_api, class_name)
+        instance = _class()
         for method_name, method_info in class_info["methods"].items():
             posargs_count = len(method_info.get("posargs", {}))
             kwargs = {k: None for k in method_info.get("kwargs", {}).keys()}
             posargs = [] + [None] * posargs_count
+            obj_under_test = _class if method_info.get("static", False) else instance
             with pytest.raises(TypeError):
                 if posargs_count > 0:
-                    getattr(instance, method_name)(*posargs[:-1])
+                    getattr(obj_under_test, method_name)(*posargs[:-1])
                 else:
-                    getattr(instance, method_name)(*([None] * len(kwargs) + 1))
-            getattr(instance, method_name)(*posargs, **kwargs)
+                    getattr(obj_under_test, method_name)(*([None] * len(kwargs) + 1))
+            getattr(obj_under_test, method_name)(*posargs, **kwargs)
