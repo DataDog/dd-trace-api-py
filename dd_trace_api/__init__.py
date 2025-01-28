88f292838ca119c5f8ae47ea46d351bdd216e377
Emmett Butler <emmett.butler321@gmail.com>
2025-01-28 07:43:01 -0800

    Revert "do not store on spans"
    This reverts commit 475535f67e07eb86cfb0422227d60ccbc501a9a7.

--- dd_trace_api/__init__.py
+++ dd_trace_api/__init__.py
@@ -75,73 +75,133 @@ class Span():
     def __enter__(self) -> 'Span':
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.__enter__", ([shared_state], {}))
-        return self
+        retval = self
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def __exit__(self, exc_type: Type[BaseException], exc_val: BaseException, exc_tb: Optional[TracebackType]) -> None:
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.__exit__", ([shared_state, exc_type, exc_val, exc_tb], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def set_exc_info(self, exc_type: Type[BaseException], exc_val: BaseException, exc_tb: Optional[TracebackType]) -> None:
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.set_exc_info", ([shared_state, exc_type, exc_val, exc_tb], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def set_link(self, trace_id: int, span_id: int, tracestate:Optional[str]=None, flags:Optional[int]=None, attributes:Optional[Dict[str, Any]]=None) -> None:
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.set_link", ([shared_state, trace_id, span_id], {'tracestate': tracestate, 'flags': flags, 'attributes': attributes}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def link_span(self, attributes:Optional[Dict[str, Any]]=None) -> None:
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.link_span", ([shared_state], {'attributes': attributes}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def set_traceback(self, limit: Optional[int]) -> None:
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.set_traceback", ([shared_state, limit], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def set_tags(self, tags: Dict[_TagNameType, Any]) -> None:
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.set_tags", ([shared_state, tags], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def set_tag_str(self, key: _TagNameType, value: Text) -> None:
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.set_tag_str", ([shared_state, key, value], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def set_struct_tag(self, key: str, value: Dict[str, Any]) -> None:
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.set_struct_tag", ([shared_state, key, value], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def finish_with_ancestors(self) -> None:
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.finish_with_ancestors", ([shared_state], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def finish(self, finish_time:Optional[float]=None) -> None:
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.finish", ([shared_state], {'finish_time': finish_time}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def set_tag(self, key: _TagNameType, value:Any=None) -> None:
         shared_state = {"stub_self": self}
         audit(_DD_HOOK_PREFIX + "Span.set_tag", ([shared_state, key], {'value': value}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
 
@@ -151,13 +211,23 @@ class data_streams():
     def set_consume_checkpoint(self, typ: str, source: str, carrier_get: Callable) -> None:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "data_streams.set_consume_checkpoint", ([shared_state, typ, source, carrier_get], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def set_produce_checkpoint(self, typ: str, target: str, carrier_set: Callable) -> None:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "data_streams.set_produce_checkpoint", ([shared_state, typ, target, carrier_set], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
 
@@ -167,13 +237,23 @@ class HTTPPropagator():
     def inject(span_context: _Context, headers: Dict[str, str], non_active_span:Optional[Span]=None) -> None:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "HTTPPropagator.inject", ([shared_state, span_context, headers], {'non_active_span': non_active_span}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     @staticmethod
     def extract(headers: Any) -> None:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "HTTPPropagator.extract", ([shared_state, headers], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
 
@@ -189,49 +269,89 @@ class Tracer():
     def flush(self) -> None:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "Tracer.flush", ([shared_state], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def set_tags(self, tags: Dict[str, str]) -> None:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "Tracer.set_tags", ([shared_state, tags], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def shutdown(self, timeout: Optional[float]) -> None:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "Tracer.shutdown", ([shared_state, timeout], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def start_span(self, name: str, child_of:Optional[Union[Span, _Context]]=None, service:Optional[str]=None, resource:Optional[str]=None, span_type:Optional[str]=None, activate:bool='False') -> Span:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "Tracer.start_span", ([shared_state, name], {'child_of': child_of, 'service': service, 'resource': resource, 'span_type': span_type, 'activate': activate}))
-        return Span()
+        retval = Span()
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def current_root_span(self) -> Span:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "Tracer.current_root_span", ([shared_state], {}))
-        return Span()
+        retval = Span()
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def current_span(self) -> Span:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "Tracer.current_span", ([shared_state], {}))
-        return Span()
+        retval = Span()
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def trace(self, name: str, service:Optional[str]=None, resource:Optional[str]=None, span_type:Optional[str]=None) -> Span:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "Tracer.trace", ([shared_state, name], {'service': service, 'resource': resource, 'span_type': span_type}))
-        return Span()
+        retval = Span()
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def wrap(self, name:Optional[str]=None, service:Optional[str]=None, resource:Optional[str]=None, span_type:Optional[str]=None) -> Callable[[AnyCallable], AnyCallable]:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "Tracer.wrap", ([shared_state], {'name': name, 'service': service, 'resource': resource, 'span_type': span_type}))
-        return lambda *args, **kwargs: None
+        retval = lambda *args, **kwargs: None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
 
@@ -241,19 +361,34 @@ class Pin():
     def onto(self, obj: Any, send:bool=True) -> None:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "Pin.onto", ([shared_state, obj], {'send': send}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def remove_from(self, obj: Any) -> None:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "Pin.remove_from", ([shared_state, obj], {}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
     def clone(self, service:Optional[str]=None, tags:Optional[Dict[str, str]]=None, tracer:Tracer=None) -> None:
         shared_state = {}
         audit(_DD_HOOK_PREFIX + "Pin.clone", ([shared_state], {'service': service, 'tags': tags, 'tracer': tracer}))
-        return None
+        retval = None
+        if retval is not None:
+            for key, value in shared_state.items():
+                if value is not self:
+                    setattr(retval, "_" + key, value)
+        return retval
         
     
 
